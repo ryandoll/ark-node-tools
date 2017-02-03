cb47d574cdf09a144b44c6c878f75a503c2c9d40
ryandoll <ryandoll@gmail.com>
2017-02-02 21:06:32 -0600

    Making a simple service to send back the ARK server status.

--- server-status.js
+++ server-status.js
@@ -1,11 +1,33 @@
 var Gamedig = require('gamedig');
-Gamedig.query(
-    {
-        type: 'arkse',
-        host: 'rhino.game-server.cc'
-    },
-    function(state) {
-        if(state.error) console.log("ARK server is offline.");
-        else console.log(state);
-    }
-);
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
+
+app.get("/ark-status", function(req, res) {
+
+  Gamedig.query(
+      {
+          type: 'arkse',
+          host: 'rhino.game-server.cc'
+      },
+      function(state) {
+          var message;
+          if(state.error){
+            message = {
+              "error" : "Server is offline."
+            }
+          } else {
+            message = state;
+          }
+
+          res.send(message);
+      }
+  );
+});
